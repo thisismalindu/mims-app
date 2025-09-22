ef6cd899e6dc4ca91a9465255e0c224b7b6b4554
Malindu Bandara <thisismalindu@gmail.com>
2025-09-22 09:01:19 +0530

    change page components

--- src/app/components/Customers.jsx
+++ src/app/components/Customers.jsx
@@ -8,12 +8,7 @@ export default function Customers() {
         <h2>Customer Details</h2>
         <p>Here you can display customer-related data.</p>
       </div>
-      <Link
-        href="./create-customer"
-        className="bg-blue-600 text-white p-4 rounded-lg cursor-pointer hover:bg-blue-700"
-      >
-        Create Customer
-      </Link>
+      
     </>
   );
 }
