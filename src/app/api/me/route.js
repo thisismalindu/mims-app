3ec79a10caad4cd744cb5c06e3ff32c268fa9e02
Malindu Bandara <thisismalindu@gmail.com>
2025-09-22 14:03:28 +0530

    modularize the get user information code

--- src/app/api/me/route.js
+++ src/app/api/me/route.js
@@ -1,20 +1,13 @@
 // src/app/api/me/route.js
 import { NextResponse } from 'next/server'
-import { jwtVerify } from 'jose'
+import { getCurrentUser } from '../utils/get-user'
 
 export async function GET(request) {
-  try {
-    const token = request.cookies.get('auth_token')?.value
-    console.log('*******************token: ', token);
-    
-    if (!token) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+  const user = await getCurrentUser(request)
 
-    // verify token
-    const { payload } = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET))
-
-    return NextResponse.json({ username: payload.username })
-
-  } catch (err) {
-    return NextResponse.json({ error: 'Invalid token' }, { status: 401 })
+  if (!user) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
   }
+
+  return NextResponse.json(user)
 }
