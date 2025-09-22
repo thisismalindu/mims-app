f5aff042562b816ccb3093b603523b1a2bf8af35
Malindu Bandara <thisismalindu@gmail.com>
2025-09-22 15:20:21 +0530

    modularize the function to use from command line with custom args

--- generate-hash.js
+++ generate-hash.js
@@ -1,9 +1,16 @@
 import bcrypt from "bcrypt";
 
-
 const run = async (password) => {
   const hash = await bcrypt.hash(password, 10); // 10 = cost factor
-console.log(`Hash for ${password}:`, hash);
+  console.log(`Hash for ${password}:`, hash);
 };
 
-run('mypassword');
+// Get password from command line arguments
+const [, , password] = process.argv;
+
+if (!password) {
+  console.log("Usage: node generate-hash.js <password>");
+  process.exit(1);
+}
+
+run(password);
