8c00e982cd50f9f2d351050be193dcff2607c794
Malindu Bandara <thisismalindu@gmail.com>
2025-10-20 17:09:32 +0530

    feat: add initial user entries for admin, agent, and manager roles in database schema

--- src/app/api/setup-database/database-schema.sql
+++ src/app/api/setup-database/database-schema.sql
@@ -277,6 +277,15 @@ BEGIN
   END LOOP;
 END$$;
 
--- root user
+-- admin user
 INSERT INTO users (username, password_hash, first_name, last_name, role, status)
-VALUES ('root', '$2b$10$LG206ujTzZxeIdIQdEbmluSMNsq0d5RrJNtfxPg8Nvz04u6xWmE36', 'Root', 'Administrator', 'admin', 'active');
+VALUES ('admin', 'adminhash', 'AdminUserPerson', 'Btrustable', 'admin', 'active');
+
+-- agent user
+INSERT INTO users (username, password_hash, first_name, last_name, role, status)
+VALUES ('agent', 'agenthash', 'AgentUserPerson', 'Btrustable', 'agent', 'active');
+
+-- manager user
+INSERT INTO users (username, password_hash, first_name, last_name, role, status)
+VALUES ('manager', 'managerhash', 'ManagerUserPerson', 'Btrustable', 'manager', 'active');
+
